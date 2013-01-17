928aa013b04d81b18507ed4bb813ca3aaa0544a2
alaint <alaint@DJF2935J.subito.de>
2013-01-17 14:01:22 +0100

    g

--- src/Power.java
+++ src/Power.java
@@ -1,5 +1,22 @@
 
 public class Power {
 
-private int name;
+private String name;
+private String vorname;
+
+public String getName() { 
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getVorname() {
+	return vorname;
+}
+
+public void setVorname(String vorname) {
+	this.vorname = vorname;
+}
 }
